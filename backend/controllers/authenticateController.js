e5db279914e4b13b1bce34d6f723a5fe15b481ed
Toky245 <aroniaina@gmail.com>
2025-07-31 10:05:42 +0300

    Update SameSite attribute for cookie settings to 'Lax' for improved security

--- backend/controllers/authenticateController.js
+++ backend/controllers/authenticateController.js
@@ -31,7 +31,7 @@ exports.login = (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'false',
-      sameSite: 'none',
+      sameSite: 'Lax',
       maxAge: 7 * 24 * 60 * 60 * 1000, 
     });
 
