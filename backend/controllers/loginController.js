8c4e89efbc9e4e4785ecc047368d0cca0c24231c
Toky245 <aroniaina@gmail.com>
2025-07-31 11:47:57 +0300

    Update SameSite attribute for cookies to 'None' for cross-site compatibility

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -47,7 +47,7 @@ exports.login = (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: true, 
-      sameSite: 'Lax', 
+      sameSite: 'None', 
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
