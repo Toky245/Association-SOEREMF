9aca77a4eca0607bcaf343e162e4c77a07d0662e
Toky245 <aroniaina@gmail.com>
2025-08-01 12:07:16 +0300

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,27 +2,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import dotenv from 'dotenv';
-import { env } from "process";
+import dotenv from "dotenv";
+
 dotenv.config();
 
 export default defineConfig(({ mode }) => ({
-  base: "./", 
+  base: "./",
   server: {
     host: "::",
     port: 8080,
   },
- define: {
-  'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
-},
-
+  define: {
+    "import.meta.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL),
+  },
   plugins: [
     react(),
     mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(path.resolve(), "./src"),
     },
   },
 }));
